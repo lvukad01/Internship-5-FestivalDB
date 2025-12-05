bc4d842a7e96d297b7be2837b9f0821f6cb7a569
lvukad01 <lvukad01@fesb.hr>
2025-12-05 10:35:15 +0100

    Finish creating triggers

--- Triggers.sql
+++ Triggers.sql
@@ -24,6 +24,9 @@ FOR EACH ROW
 EXECUTE FUNCTION check_performance_overlap();
 
 
+
+
+
 CREATE OR REPLACE FUNCTION check_performance_visitors()
 RETURNS TRIGGER AS $$
 DECLARE stageCapacity INT;
@@ -46,6 +49,9 @@ CREATE TRIGGER trg_check_performance_visitors
 BEFORE INSERT OR UPDATE ON Performances
 FOR EACH ROW
 EXECUTE FUNCTION check_performance_visitors();
+
+
+
 
 CREATE OR REPLACE FUNCTION check_festival_capacity()
 RETURNS TRIGGER AS $$
@@ -77,6 +83,9 @@ FOR EACH ROW
 EXECUTE FUNCTION check_festival_capacity();
 
 
+
+
+
 CREATE OR REPLACE FUNCTION update_total_price()
 RETURNS TRIGGER AS $$
 DECLARE total FLOAT;
@@ -97,4 +106,135 @@ CREATE TRIGGER trg_update_total_price
 BEFORE INSERT OR UPDATE OR DELETE ON PurchaseItems
 FOR EACH ROW
 EXECUTE FUNCTION update_total_price();
+
+
+
+
+CREATE OR REPLACE FUNCTION check_membership_card()
+RETURNS TRIGGER AS $$
+BEGIN
+
+	IF EXISTS (
+		SELECT 1
+		FROM MembershipCard
+		WHERE VisitorID = NEW.VisitorID
+    ) THEN
+        RAISE EXCEPTION 'Posjetitelj već ima Membership Card.';
+    END IF;
+	IF NEW.ActivationDate > NOW() THEN
+        RAISE EXCEPTION 'Datum aktivacije ne može biti u budućnosti.';
+	END IF
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_check_membership_card
+BEFORE INSERT ON MembershipCard
+FOR EACH ROW
+EXECUTE FUNCTION check_membership_card();
+
+
+
+
+CREATE OR UPDATE assign_membership_card()
+RETURNS TRIGGER AS $$
+DECLARE festivalCount INT;
+DECLARE totalSpent FLOAT;
+BEGIN
+	SELECT COUNT(DISTINCT FestivalID ) INTO festivalCount
+	FROM Purchases
+	WHERE VisitorID=NEW.VisitorID;
 	
+	SELECT COALESCE(SUM(TotalPrice),0) INTO totalSpent
+    FROM Purchases
+    WHERE VisitorID = NEW.VisitorID;
+    IF festival_count > 3 AND total_spent > 600 THEN
+        IF NOT EXISTS (
+            SELECT 1 FROM MembershipCard
+            WHERE VisitorID = NEW.VisitorID
+        ) THEN
+            INSERT INTO MembershipCard(ActivationDate, Status, VisitorID)
+            VALUES (NOW(), 'aktivan', NEW.VisitorID);
+        END IF;
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_assign_membership_card
+AFTER INSERT ON Purchases
+FOR EACH ROW
+EXECUTE FUNCTION assign_membership_card();
+	
+
+
+
+CREATE OR REPLACE FUNCTION check_workshop_registration()
+RETURNS TRIGGER AS $$
+DECLARE currentParticipants INT;
+DECLARE requiredKnowledge BOOLEAN;
+BEGIN
+    SELECT COUNT(*) INTO currentParticipants
+    FROM VisitorWorkshops
+    WHERE WorkshopID = NEW.WorkshopID;
+
+    SELECT PriorKnowledge INTO requiredKnowledge
+    FROM Workshops
+    WHERE WorkshopID = NEW.WorkshopID;
+
+    IF currentParticipants >= (SELECT Capacity FROM Workshops WHERE WorkshopID = NEW.WorkshopID) THEN
+        RAISE EXCEPTION 'Radionica je popunjena. Maksimalni broj polaznika je %', 
+            (SELECT Capacity FROM Workshops WHERE WorkshopID = NEW.WorkshopID);
+    END IF;
+
+    IF requiredKnowledge AND NEW.Status = 'prijavljen' THEN
+        IF NOT NEW.HasPriorKnowledge THEN
+            RAISE EXCEPTION 'Radionica zahtijeva prethodno znanje.';
+        END IF;
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_check_workshop_registration
+BEFORE INSERT OR UPDATE ON VisitorWorkshops
+FOR EACH ROW
+EXECUTE FUNCTION check_workshop_registration();
+
+
+
+CREATE OR REPLACE FUNCTION check_staff_festival_overlap()
+RETURNS TRIGGER AS $$
+DECLARE existingStart TIMESTAMP;
+DECLARE existingEnd TIMESTAMP;
+BEGIN
+    SELECT f.StartDate, f.EndDate INTO existingStart, existingEnd
+    FROM Festivals f
+    JOIN Staff s ON s.FestivalID = f.FestivalID
+    WHERE s.StaffID = NEW.StaffID;
+
+    IF FOUND THEN
+        IF (NEW.FestivalID IS NOT NULL) THEN
+            DECLARE newStart TIMESTAMP;
+            DECLARE newEnd TIMESTAMP;
+            SELECT StartDate, EndDate
+            INTO newStart, newEnd
+            FROM Festivals
+            WHERE FestivalID = NEW.FestivalID;
+
+            IF (newStart <= existingEnd AND newEnd >= existingStart) THEN
+                RAISE EXCEPTION 'Osoba već radi na festivalu u istom periodu.';
+            END IF;
+        END IF;
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_check_staff_festival_overlap
+BEFORE INSERT OR UPDATE ON Staff
+FOR EACH ROW
+EXECUTE FUNCTION check_staff_festival_overlap();
